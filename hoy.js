bfb02f420aba7017d57d89238b25c32e3fcb1a2c
Bloatyy <kunalsrathore99@gmail.com>
2026-04-20 00:33:53 +0530

    Delete hoy.js

--- hoy.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.faq-q').forEach(btn => {
-  btn.addEventListener('click', () => {
-    const answer = btn.nextElementSibling;
-    const isOpen = answer.classList.contains('open');
-    document.querySelectorAll('.faq-a').forEach(a => a.classList.remove('open'));
-    document.querySelectorAll('.faq-q').forEach(b => b.classList.remove('open'));
-    if (!isOpen) { answer.classList.add('open'); btn.classList.add('open'); }
-  });
-});
